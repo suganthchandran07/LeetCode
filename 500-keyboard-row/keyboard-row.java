fe542f261d2cabfc164dedd0d142e65a2683a3bb
Suganth <suganthchandran.a@gmail.com>
2026-01-07 00:49:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (69.29%) - LeetSync

--- 500-keyboard-row/keyboard-row.java
+++ 500-keyboard-row/keyboard-row.java
@@ -1,22 +1,46 @@
 class Solution {
     public String[] findWords(String[] words) {
-        String a = "qwertyuiop";
-        String b = "asdfghjkl";
-        String c = "zxcvbnm";
+        int[] row = new int[26];
+        for (char a : "qwertyuiop".toCharArray()) row[a - 'a'] = 1;
+        for (char a : "asdfghjkl".toCharArray()) row[a - 'a'] = 2;
+        for (char a : "zxcvbnm".toCharArray()) row[a - 'a'] = 3;
         List<String> res = new ArrayList<>();
         for (String word : words) {
-            int aCount = 0, bCount = 0, cCount = 0;
-            for (char wd : word.toLowerCase().toCharArray()) {
-                String w = String.valueOf(wd);
-                if (a.contains(w)) aCount++;
-                if (b.contains(w)) bCount++;
-                if (c.contains(w)) cCount++;
+            String lower = word.toLowerCase();
+            int a = row[lower.charAt(0) - 'a'];
+            boolean valid = true;
+            for (int i = 0; i < lower.length(); i++) {
+                if (row[lower.charAt(i) - 'a'] != a) {
+                    valid = false;
+                    break;
+                }
             }
-            if (!(aCount != 0 && bCount != 0) 
-                    && !(bCount != 0 && cCount != 0) 
-                    && !(cCount != 0 && aCount != 0))
-                res.add(word);
+            if (valid) res.add(word);
         }
         return res.toArray(new String[0]);
+
+        // String a = "qwertyuiop";
+        // String b = "asdfghjkl";
+        // String c = "zxcvbnm";
+        // List<String> res = new ArrayList<>();
+        // for (String word : words) {
+        //     int aCount = 0, bCount = 0, cCount = 0;
+        //     for (char wd : word.toLowerCase().toCharArray()) {
+        //         String w = String.valueOf(wd);
+        //         if (a.contains(w)) aCount++;
+        //         if (b.contains(w)) bCount++;
+        //         if (c.contains(w)) cCount++;
+        //     }
+        //     if (!(aCount != 0 && bCount != 0) 
+        //             && !(bCount != 0 && cCount != 0) 
+        //             && !(cCount != 0 && aCount != 0))
+        //         res.add(word);
+        // }
+        // String[] ans = new String[res.size()];
+        // int index = 0;
+        // for (String word : res) {
+        //     ans[index++] = word;
+        // }
+        // return ans;
     }
 }
