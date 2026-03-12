574cf846b6d2d634eeee1bd77e7941b60db1df03
Suganth <suganthchandran.a@gmail.com>
2026-03-13 01:25:08 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (89.64%) - LeetSync

--- 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.java
+++ 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.java
@@ -5,8 +5,10 @@ class Solution {
         for (int i = 0; i < n; i++) {
             res.add(index[i], nums[i]);
         }
-        return res.stream()
-            .mapToInt(Integer::intValue)
-            .toArray();
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = res.get(i);
+        }
+        return ans;
     }
 }
