57956fc0aa460517ad9aed16363d9344abc44d86
Suganth <suganthchandran.a@gmail.com>
2026-03-15 17:35:01 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (88.36%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.java
+++ 566-reshape-the-matrix/reshape-the-matrix.java
@@ -1,20 +1,37 @@
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
+        // int m = mat.length;
+        // int n = mat[0].length;
+        // if (m*n != r*c) return mat;
+        // int[] ans = new int[m*n];
+        // int k = 0;
+        // for (int i = 0; i < m; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         ans[k++] = mat[i][j];
+        //     }
+        // }
+        // k = 0;
+        // int[][] res = new int[r][c];
+        // for (int i = 0; i < r; i++) {
+        //     for (int j = 0; j < c; j++) {
+        //         res[i][j] = ans[k++];
+        //     }
+        // }
+        // return res;
+
         int m = mat.length;
         int n = mat[0].length;
-        if (m*n != r*c) return mat;
-        int[] ans = new int[m*n];
-        int k = 0;
+        int row = 0, col = 0;
+        if (r*c != m*n) return mat;
+        int[][] res = new int[r][c];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                ans[k++] = mat[i][j];
-            }
-        }
-        k = 0;
-        int[][] res = new int[r][c];
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                res[i][j] = ans[k++];
+                res[row][col] = mat[i][j];
+                col++;
+                if (col == c) {
+                    col = 0;
+                    row++;
+                }
             }
         }
         return res;
