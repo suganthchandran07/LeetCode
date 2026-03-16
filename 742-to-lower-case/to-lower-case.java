96605e68aa3a9be3149f838fa316a7a7372913d0
Suganth <suganthchandran.a@gmail.com>
2026-03-16 21:14:30 +0530

    Time: 1 ms (22.95%) | Memory: 42.8 MB (80.65%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,5 +1,10 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        // return s.toLowerCase();
+        StringBuilder str = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            str.append(Character.toLowerCase(c));
+        }
+        return str.toString();
     }
 }
