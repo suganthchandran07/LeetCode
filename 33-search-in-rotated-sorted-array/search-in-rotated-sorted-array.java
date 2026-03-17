dc894e6812f61da2074c151b871a1433f9cf5811
Suganth <suganthchandran.a@gmail.com>
2026-03-17 19:13:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (78.62%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,23 +1,29 @@
 class Solution {
     public int search(int[] nums, int target) {
         int pivot = findPivot(nums);
-        int firstSearch = binary(nums, target, 0, pivot);
-        if (firstSearch != -1) return firstSearch;
-        return binary(nums, target, pivot + 1, nums.length - 1);
+        if (pivot == -1)
+            return binary(nums, target, 0, nums.length - 1);
+        if (nums[pivot] == target)
+            return pivot;
+        if (nums[0] > target)
+            return binary(nums, target, pivot + 1, nums.length - 1);
+        return binary(nums, target, 0, pivot - 1);
     }
 
     private int findPivot(int[] nums) {
         int left = 0, right = nums.length - 1;
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] > nums[mid + 1])
+            if (mid < right && nums[mid] > nums[mid + 1])
                 return mid;
-            else if (nums[left] > nums[mid])
+            else if (mid > left && nums[mid] < nums[mid - 1])
+                return mid - 1;
+            else if (nums[mid] < nums[left])
                 right = mid - 1;
             else
                 left = mid + 1;
         }
-        return left;
+        return -1;
     }
 
      private int binary(int[] nums, int target, int left, int right) {
